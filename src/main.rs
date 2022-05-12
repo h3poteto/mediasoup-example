faeee645f912bad8dea5cc1820e477f74e88208d
AkiraFukushima <h3.poteto@gmail.com>
2022-05-13 00:05:17 +0900

    Implement basic sfu server

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,399 @@
-use actix_web::{get, web, Responder, HttpResponse, HttpServer, App, HttpRequest, Error};
-use actix::{Actor, StreamHandler};
+use actix::prelude::Message;
+use actix::{Actor, ActorContext, AsyncContext, Handler, StreamHandler};
+use actix_web::web::Data;
+use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web_actors::ws;
+use mediasoup::prelude::{
+    Consumer, ConsumerId, ConsumerOptions, DtlsParameters, IceCandidate, IceParameters,
+    MimeTypeAudio, Producer, ProducerId, Router, RouterOptions, RtcpFeedback, RtpCapabilities,
+    RtpCapabilitiesFinalized, RtpCodecCapability, RtpCodecParametersParameters, Transport,
+    TransportId, TransportListenIp, TransportListenIps, WebRtcTransport, WebRtcTransportOptions,
+    WebRtcTransportRemoteParameters, WorkerManager, WorkerSettings,
+};
+use mediasoup::producer::ProducerOptions;
+use mediasoup::rtp_parameters::{MediaKind, RtpParameters};
+use mediasoup::worker::{WorkerLogLevel, WorkerLogTag};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::num::{NonZeroU32, NonZeroU8};
 
-struct ExampleWS;
-
-impl Actor for ExampleWS {
-    type Context = ws::WebsocketContext<Self>;
+fn media_codecs() -> Vec<RtpCodecCapability> {
+    vec![RtpCodecCapability::Audio {
+        mime_type: MimeTypeAudio::Opus,
+        preferred_payload_type: None,
+        clock_rate: NonZeroU32::new(48000).unwrap(),
+        channels: NonZeroU8::new(2).unwrap(),
+        parameters: RtpCodecParametersParameters::from([("useinbandfec", 1_u32.into())]),
+        rtcp_feedback: vec![RtcpFeedback::TransportCc],
+    }]
 }
 
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ExampleWS {
+struct Transports {
+    consumer: WebRtcTransport,
+    producer: WebRtcTransport,
+}
+
+struct ExampleConn {
+    client_rtp_capabilities: Option<RtpCapabilities>,
+    consumers: HashMap<ConsumerId, Consumer>,
+    producers: Vec<Producer>,
+    router: Router,
+    transports: Transports,
+}
+
+impl ExampleConn {
+    async fn new(worker_manager: &WorkerManager) -> Result<Self, String> {
+        let worker = worker_manager
+            .create_worker({
+                let mut settings = WorkerSettings::default();
+                settings.log_level = WorkerLogLevel::Debug;
+                settings.log_tags = vec![
+                    WorkerLogTag::Info,
+                    WorkerLogTag::Ice,
+                    WorkerLogTag::Dtls,
+                    WorkerLogTag::Rtp,
+                    WorkerLogTag::Srtp,
+                    WorkerLogTag::Rtcp,
+                    WorkerLogTag::Rtx,
+                    WorkerLogTag::Bwe,
+                    WorkerLogTag::Score,
+                    WorkerLogTag::Simulcast,
+                    WorkerLogTag::Svc,
+                    WorkerLogTag::Sctp,
+                    WorkerLogTag::Message,
+                ];
+
+                settings
+            })
+            .await
+            .map_err(|error| format!("Failed to create worker: {}", error))?;
+        let router = worker
+            .create_router(RouterOptions::new(media_codecs()))
+            .await
+            .map_err(|error| format!("Failed to create router: {}", error))?;
+
+        // This example uses only 2 transports.
+        let transport_options =
+            WebRtcTransportOptions::new(TransportListenIps::new(TransportListenIp {
+                ip: "127.0.0.1".parse().unwrap(),
+                announced_ip: None,
+            }));
+        let producer_transport = router
+            .create_webrtc_transport(transport_options.clone())
+            .await
+            .map_err(|error| format!("Failed to create producer transport: {}", error))?;
+        let consumer_transport = router
+            .create_webrtc_transport(transport_options.clone())
+            .await
+            .map_err(|error| format!("Failed to create consumer transport: {}", error))?;
+
+        Ok(Self {
+            client_rtp_capabilities: None,
+            consumers: HashMap::new(),
+            producers: vec![],
+            router: router,
+            transports: Transports {
+                producer: producer_transport,
+                consumer: consumer_transport,
+            },
+        })
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct TransportOptions {
+    id: TransportId,
+    dtls_parameters: DtlsParameters,
+    ice_candidates: Vec<IceCandidate>,
+    ice_parameters: IceParameters,
+}
+
+#[derive(Serialize, Message)]
+#[serde(tag = "action")]
+#[rtype(result = "()")]
+enum ServerMessage {
+    #[serde(rename_all = "camelCase")]
+    Init {
+        consumer_transport_options: TransportOptions,
+        producer_transport_options: TransportOptions,
+        router_rtp_capabilities: RtpCapabilitiesFinalized,
+    },
+
+    ConnectedProducerTransport,
+    #[serde(rename_all = "camelCase")]
+    Produced {
+        id: ProducerId,
+    },
+    ConnectedConsumerTransport,
+    #[serde(rename_all = "camelCase")]
+    Consumed {
+        id: ConsumerId,
+        producer_id: ProducerId,
+        kind: MediaKind,
+        rtp_parameters: RtpParameters,
+    },
+}
+
+#[derive(Deserialize, Message)]
+#[serde(tag = "action")]
+#[rtype(result = "()")]
+enum ClientMessage {
+    #[serde(rename_all = "camelCase")]
+    Init { rtp_capabilities: RtpCapabilities },
+    #[serde(rename_all = "camelCase")]
+    ConnectProducerTransport { dtls_parameters: DtlsParameters },
+    #[serde(rename_all = "camelCase")]
+    Produce {
+        kind: MediaKind,
+        rtp_parameters: RtpParameters,
+    },
+    #[serde(rename_all = "camelCase")]
+    ConnectConsumerTransport { dtls_parameters: DtlsParameters },
+    #[serde(rename_all = "camelCase")]
+    Consume { producer_id: ProducerId },
+    #[serde(rename_all = "camelCase")]
+    ConsumerResume { id: ConsumerId },
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+enum InternalMessage {
+    SaveProducer(Producer),
+    SaveConsumer(Consumer),
+    Stop,
+}
+
+impl Actor for ExampleConn {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        println!("WebSocket connection created");
+
+        let server_init_message = ServerMessage::Init {
+            consumer_transport_options: TransportOptions {
+                id: self.transports.consumer.id(),
+                dtls_parameters: self.transports.consumer.dtls_parameters(),
+                ice_candidates: self.transports.consumer.ice_candidates().clone(),
+                ice_parameters: self.transports.consumer.ice_parameters().clone(),
+            },
+            producer_transport_options: TransportOptions {
+                id: self.transports.producer.id(),
+                dtls_parameters: self.transports.producer.dtls_parameters(),
+                ice_candidates: self.transports.producer.ice_candidates().clone(),
+                ice_parameters: self.transports.producer.ice_parameters().clone(),
+            },
+            router_rtp_capabilities: self.router.rtp_capabilities().clone(),
+        };
+
+        ctx.address().do_send(server_init_message);
+    }
+
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        println!("WebSocket connection closed");
+    }
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ExampleConn {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
             Ok(ws::Message::Ping(msg)) => {
                 println!("receive pong {:?}", msg);
                 ctx.pong(&msg)
-            },
-            Ok(ws::Message::Text(text)) => {
-                println!("receive text {:?}", text);
-                ctx.text(text)
+            }
+            Ok(ws::Message::Pong(_)) => {}
+            Ok(ws::Message::Text(text)) => match serde_json::from_str::<ClientMessage>(&text) {
+                Ok(message) => {
+                    ctx.address().do_send(message);
+                }
+                Err(error) => {
+                    eprintln!("Failed to parse client message: {}\n{}", error, text);
+                }
             },
             Ok(ws::Message::Binary(bin)) => {
                 println!("receive binary {:?}", bin);
                 ctx.binary(bin)
-            },
+            }
+            Ok(ws::Message::Close(reason)) => ctx.close(reason),
             _ => (),
         }
     }
 }
 
-async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(ExampleWS, &req, stream);
-    println!("{:?}", resp);
-    resp
+impl Handler<ClientMessage> for ExampleConn {
+    type Result = ();
+
+    fn handle(&mut self, message: ClientMessage, ctx: &mut Self::Context) {
+        match message {
+            ClientMessage::Init { rtp_capabilities } => {
+                self.client_rtp_capabilities.replace(rtp_capabilities);
+            }
+            ClientMessage::ConnectProducerTransport { dtls_parameters } => {
+                let address = ctx.address();
+                let transport = self.transports.producer.clone();
+                actix::spawn(async move {
+                    match transport
+                        .connect(WebRtcTransportRemoteParameters { dtls_parameters })
+                        .await
+                    {
+                        Ok(_) => {
+                            address.do_send(ServerMessage::ConnectedProducerTransport);
+                            println!("Producer transport connected");
+                        }
+                        Err(error) => {
+                            eprintln!("Failed to connect producer transport: {}", error);
+                            address.do_send(InternalMessage::Stop);
+                        }
+                    }
+                });
+            }
+            ClientMessage::Produce {
+                kind,
+                rtp_parameters,
+            } => {
+                let address = ctx.address();
+                let transport = self.transports.producer.clone();
+
+                actix::spawn(async move {
+                    match transport
+                        .produce(ProducerOptions::new(kind, rtp_parameters))
+                        .await
+                    {
+                        Ok(producer) => {
+                            let id = producer.id();
+                            address.do_send(ServerMessage::Produced { id });
+                            address.do_send(InternalMessage::SaveProducer(producer));
+                            println!("{:?} producer created: {}", kind, id);
+                        }
+                        Err(error) => {
+                            eprintln!("Failed to create {:?} producer: {}", kind, error);
+                            address.do_send(InternalMessage::Stop);
+                        }
+                    }
+                });
+            }
+            ClientMessage::ConnectConsumerTransport { dtls_parameters } => {
+                let address = ctx.address();
+                let transport = self.transports.consumer.clone();
+
+                actix::spawn(async move {
+                    match transport
+                        .connect(WebRtcTransportRemoteParameters { dtls_parameters })
+                        .await
+                    {
+                        Ok(_) => {
+                            address.do_send(ServerMessage::ConnectedConsumerTransport);
+                            println!("Consumer transport connected");
+                        }
+                        Err(error) => {
+                            eprintln!("Failed to connect consumer transport: {}", error);
+                            address.do_send(InternalMessage::Stop);
+                        }
+                    }
+                });
+            }
+            ClientMessage::Consume { producer_id } => {
+                let address = ctx.address();
+                let transport = self.transports.consumer.clone();
+                let rtp_capabilities = match self.client_rtp_capabilities.clone() {
+                    Some(rtp_capabilities) => rtp_capabilities,
+                    None => {
+                        eprintln!("Client should send RTP capabilities before consuming, for example Init");
+                        return;
+                    }
+                };
+
+                actix::spawn(async move {
+                    let mut options = ConsumerOptions::new(producer_id, rtp_capabilities);
+                    options.paused = true;
+
+                    match transport.consume(options).await {
+                        Ok(consumer) => {
+                            let id = consumer.id();
+                            let kind = consumer.kind();
+                            let rtp_parameters = consumer.rtp_parameters().clone();
+                            address.do_send(ServerMessage::Consumed {
+                                id,
+                                producer_id,
+                                kind,
+                                rtp_parameters,
+                            });
+                            address.do_send(InternalMessage::SaveConsumer(consumer));
+                            println!("{:?} consumer created: {}", kind, id);
+                        }
+                        Err(error) => {
+                            eprintln!("Failed to create consumer: {}", error);
+                            address.do_send(InternalMessage::Stop);
+                        }
+                    }
+                });
+            }
+            ClientMessage::ConsumerResume { id } => {
+                if let Some(consumer) = self.consumers.get(&id).cloned() {
+                    actix::spawn(async move {
+                        match consumer.resume().await {
+                            Ok(_) => {
+                                println!(
+                                    "Successflly resumed {:?} consumer {}",
+                                    consumer.kind(),
+                                    consumer.id()
+                                );
+                            }
+                            Err(error) => {
+                                println!(
+                                    "Failed to resume {:?} consumer {}: {}",
+                                    consumer.kind(),
+                                    consumer.id(),
+                                    error
+                                );
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
+
+impl Handler<ServerMessage> for ExampleConn {
+    type Result = ();
+
+    fn handle(&mut self, message: ServerMessage, ctx: &mut Self::Context) {
+        ctx.text(serde_json::to_string(&message).unwrap());
+    }
+}
+
+impl Handler<InternalMessage> for ExampleConn {
+    type Result = ();
+
+    fn handle(&mut self, message: InternalMessage, ctx: &mut Self::Context) {
+        match message {
+            InternalMessage::Stop => {
+                ctx.stop();
+            }
+            InternalMessage::SaveProducer(producer) => {
+                self.producers.push(producer);
+            }
+            InternalMessage::SaveConsumer(consumer) => {
+                self.consumers.insert(consumer.id(), consumer);
+            }
+        }
+    }
+}
+
+async fn index(
+    req: HttpRequest,
+    worker_manager: Data<WorkerManager>,
+    stream: web::Payload,
+) -> Result<HttpResponse, Error> {
+    match ExampleConn::new(&worker_manager).await {
+        Ok(server) => ws::start(server, &req, stream),
+        Err(error) => {
+            eprintln!("{}", error);
+
+            Ok(HttpResponse::InternalServerError().finish())
+        }
+    }
 }
 
 #[get("/")]
@@ -41,12 +403,15 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let worker_manager = Data::new(WorkerManager::new());
+    HttpServer::new(move || {
         App::new()
             .service(hello)
+            .app_data(worker_manager.clone())
             .route("/ws", web::get().to(index))
     })
-        .bind("127.0.0.1:3000")?
-        .run()
-        .await
+    .workers(2)
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
